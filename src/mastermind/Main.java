a8a23c4f6216c05ce8570ceaa82c4e8b54ce7d23
Moamen Mohammed <150467820+moa2004@users.noreply.github.com>
2024-12-06 19:37:06 +0200

    Add files via upload

---
+++ src/mastermind/Main.java
@@ -0,0 +1,32 @@
+package mastermind;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        GameController controller = new GameController();
+        controller.startTimer();
+        VBox layout = controller.createGameLayout();
+        Scene scene = new Scene(layout, 800, 600);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/mastermind/mastermind.css")).toExternalForm());
+        primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/mastermind/pixelcut-export-min.png"))));
+        primaryStage.setTitle("Mastermind Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
